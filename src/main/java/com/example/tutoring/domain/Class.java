6b87e83b1d37d69e74c01e02c641ce4d74b1d697
zzansol <chsk9912@gmail.com>
2024-12-11 21:43:27 +0900

    :sparkles: feat: Student, Tutor, Class 기본 컬럼 추가
    Student, Tutor, Class 기본 컬럼 추가
    
    Fixes: #1

--- src/main/java/com/example/tutoring/domain/Class.java
+++ src/main/java/com/example/tutoring/domain/Class.java
@@ -1,4 +1,31 @@
 package com.example.tutoring.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Class extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(nullable = false)
+    private Duration duration;
+
+
+
+
 }
--- src/main/java/com/example/tutoring/domain/Student.java
+++ src/main/java/com/example/tutoring/domain/Student.java
@@ -1,4 +1,18 @@
 package com.example.tutoring.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 public class Student extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
 }
